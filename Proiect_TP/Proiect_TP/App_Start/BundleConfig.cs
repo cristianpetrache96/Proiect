eb1cdc8da89422692c212bfd1e8e48ab3dbe2fce
Christian <cristian.petrache@yahoo.com>
2017-11-08 08:22:21 -0800

    ChessGame class + View and Controller

--- Proiect_TP/Proiect_TP/App_Start/BundleConfig.cs
+++ Proiect_TP/Proiect_TP/App_Start/BundleConfig.cs
@@ -26,6 +26,7 @@ namespace Proiect_TP
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+            
         }
     }
 }
